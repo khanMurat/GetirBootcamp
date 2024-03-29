2883e5f829b2dc52cea207be28519616f6837b13
khanMurat <ala.muraat07@gmail.com>
2024-03-29 22:53:25 +0300

    Homework for week 2

--- Hafta2/Getir-Basic-Components/Getir-Basic-Components/Tabbar/TabbarViewController.swift
+++ Hafta2/Getir-Basic-Components/Getir-Basic-Components/Tabbar/TabbarViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class TabbarViewController: UIViewController {
+    
+    var middleButton = UIButton()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,8 +35,28 @@ class TabbarViewController: UIViewController {
         
         //orta buton hafif önde olacak şekilde nasıl yapılıyor örnek yapınız.
         
+        setupMiddleButton()
         
         // itemlar çok altta normalde daha yukarda durmaz mı? Bunu nasıl yapıyoruz
         UITabBarItem.appearance().titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -10)
+    }
+    
+    func setupMiddleButton() {
+        
+        middleButton = UIButton(frame: CGRect(x: (self.view.bounds.width / 2)-25, y: 0, width: 50, height: 50))
+
+        middleButton.backgroundColor = .gray
+        middleButton.layer.cornerRadius = middleButton.frame.height / 2
+        middleButton.layer.masksToBounds = true
+        middleButton.setImage(UIImage(systemName: "plus")!.withTintColor(.red), for: .normal)
+        middleButton.addTarget(self, action: #selector(middleButtonAction), for: .touchUpInside)
+
+        guard let tabBar = self.tabBarController?.tabBar else { return }
+        tabBar.addSubview(middleButton)
+    }
+    
+    @objc private func middleButtonAction() {
+        
+        print("Middle Button Pressed")
     }
 }
